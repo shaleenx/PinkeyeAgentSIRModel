4813eecb0228dfd93f3b169fabfca5ecf2237e10
Yashodhan Mohan Bhatnagar <yashodhan.mohan@gmail.com>
2016-04-05 17:48:40 +0530

    Universe limits added

---
+++ limits.py
@@ -0,0 +1,56 @@
+universe = {
+	'farm0': {
+		'minX': 0,
+		'maxX': 15,
+		'minY': 0,
+		'maxY': 94
+	},
+	'farm1': {
+		'minX': 17,
+		'maxX': 32,
+		'minY': 0,
+		'maxY': 94
+	},
+	'farm2': {
+		'minX': 35,
+		'maxX': 50,
+		'minY': 0,
+		'maxY': 94
+	},
+	'farm3': {
+		'minX': 52,
+		'maxX': 67,
+		'minY': 0,
+		'maxY': 94
+	},
+	'farm4': {
+		'minX': 69,
+		'maxX': 84,
+		'minY': 0,
+		'maxY': 94
+	},
+	'farm5': {
+		'minX': 71,
+		'maxX': 86,
+		'minY': 0,
+		'maxY': 94
+	},
+	'stocker': {
+		'minX': 0,
+		'maxX': 29,
+		'minY': 96,
+		'maxY': 125
+	},
+	'salebarn': {
+		'minX': 30,
+		'maxX': 32,
+		'minY': 96,
+		'maxY': 125
+	},
+	'feedlot': {
+		'minX': 33,
+		'maxX': 52,
+		'minY': 96,
+		'maxY': 125
+	}
+}
