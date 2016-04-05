20698e95e1200c10cac51237b24c775ed4073ac9
Shaleen <shaleenx@gmail.com>
2016-04-05 12:13:59 +0530

    Correct Matlab Code

--- cattleScheduler.m
+++ cattleScheduler.m
@@ -1,24 +1,24 @@
 function cattleScheduler(entity)
     if entity.onFarm == 1 & entity.weight < 600
-        // call sir
-        // call inFarm
+        % call sir
+        % call inFarm
     elseif entity.onFarm == 1
-        // call sir
-        // call farm2sale
+        % call sir
+        % call farm2sale
     elseif entity.weight < 900 & entity.inSaleBarn == 1
-        // call sir
-        // call inSalebarn1
+        % call sir
+        % call inSalebarn1
     elseif entity.onStocker == 1
-        // call sir
-        // call inStocker
+        % call sir
+        % call inStocker
     elseif entity.weight >= 900 & entity.inSaleBarn == 1
-        // call sir
-        // call inSalebarn2
+        % call sir
+        % call inSalebarn2
     elseif entity.onFeedlot == 1
-        // call sir
-        // call inFeedlot
+        % call sir
+        % call inFeedlot
     elseif entity.inAbattoir == 1
-        // call sirAbattoir
+        % call sirAbattoir
         
         entity.east = 1;
         entity.west = 0;
